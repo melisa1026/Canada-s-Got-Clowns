6470924e94817af3c90b3f3cecbfadc78f69e65b
melisa1026 <melipan01@gmail.com>
2024-01-27 17:59:05 -0500

    cursor

--- Assets/Scripts/MyCursor.cs
+++ Assets/Scripts/MyCursor.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class MyCursor : MonoBehaviour
 {
     public GameObject myCursor;
+    public Vector3 originalScale;
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
+        originalScale = myCursor.transform.localScale;
     }
 
     // Update is called once per frame
@@ -17,4 +19,15 @@ public class MyCursor : MonoBehaviour
     {
         myCursor.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x + 0.3f, Camera.main.ScreenToWorldPoint(Input.mousePosition).y - 0.5f, 0);
     }
+
+    public void bigCursor()
+    {
+        myCursor.transform.localScale = new Vector3(myCursor.transform.localScale.x * 1.5f, myCursor.transform.localScale.y * 1.5f, myCursor.transform.localScale.z);
+    }
+
+    public void normalCursor()
+    {
+        myCursor.transform.localScale = originalScale;
+    }
+
 }
