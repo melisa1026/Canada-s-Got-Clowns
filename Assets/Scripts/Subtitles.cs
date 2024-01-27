cdcf63f8f5d27a4d725f1ac289622a37e768fa8a
sonyapan1026 <107889587+sonyapan1026@users.noreply.github.com>
2024-01-27 18:34:56 -0500

    talking

---
+++ Assets/Scripts/Subtitles.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Subtitles : MonoBehaviour
+{
+
+    public GameObject subtitles;
+
+    public void OnButtonPress(string text)
+    {
+        subtitles.GetComponent<TMPro.TextMeshProUGUI>().text = text;
+    }
+
+    public void OnButtonPress()
+    {
+        StartCoroutine(subtitlesOff());
+    }
+
+    IEnumerator subtitlesOff()
+    {
+        yield return new WaitForSeconds(3f);
+        subtitles.GetComponent<TMPro.TextMeshProUGUI>().text = "";
+    }
+}
